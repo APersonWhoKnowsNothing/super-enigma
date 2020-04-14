f5f1271ec7f45ca7523b3bc651cdb68d7c01b242
PastaBoi <fbrittingham04@gmail.com>
2020-04-14 01:15:18 -0400

    emergency save

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 # Imports
 import pygame
 import time
+from pathlib import Path
+import os
 
 # Colors
 black = (0, 0, 0)
@@ -30,6 +32,31 @@ icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
 # Meat
+def button(msg, x, y, w, h, ic, ac):
+    mouse = pygame.mouse.get_pos()
+    
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(display, ac, (x, y, w, h))
+    else:
+        pygame.draw.rect(display, ic, (x, y, w, h))
+
+    text_display(msg, 15)
+
+def audio_folder():
+    path = "Audio_Files"
+    isdir = os.path.isdir(path)
+    if isdir == True:
+        return True
+        # audio_files = True
+    elif isdir == False:
+        print("`Audio_Files` Directory Does Not Exist")
+        print("Creating New Directory")
+        os.mkdir("Audio_Files")
+        print("Directory Created Successfully")
+        # audio_files = True
+    else:
+        print("fuck")
+
 def program_exit():
     pygame.quit()
     quit()
@@ -43,14 +70,14 @@ def text_object(msg, font, color):
     text_surface = font.render(msg, True, color)
     return text_surface, text_surface.get_rect()
 
-def text_display(msg):
-    text = pygame.font.Font('freesansbold.ttf', 60)
+def text_display(msg, font_size):
+    text = pygame.font.Font('freesansbold.ttf', font_size)
     text_surf, text_rect = text_object(msg, text, white)
     text_rect.center = ((display_width / 2), (35))
     display.blit(text_surf, text_rect)
 
     pygame.display.update()
-    time.sleep(2)
+    time.sleep(1)
     program_run()
 
 def program_run():
@@ -59,10 +86,14 @@ def program_run():
             if event.type == pygame.QUIT:
                 print(event)
                 program_exit()
-            
+        
+        audio_folder()
+
+        #button('test', 155, 155, 100, 50, red, green)
+
         # GUI
         background()
-        text_display("Audio Files") # Main Header
+        text_display("Audio Files", 60) # Main Header
 
         # Update Display
         pygame.display.update()
